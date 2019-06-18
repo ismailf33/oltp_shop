42ce0a748b7404db5a406f6adab6a3ab98b5181a
alif33 <alifhasan332@gmail.com>
2019-06-18 23:48:00 +0600

    @Category inserted successfully .

--- admin/catadd.php
+++ admin/catadd.php
@@ -1,14 +1,30 @@
-﻿<?php include 'inc/header.php';?>
-<?php include 'inc/sidebar.php';?>
+﻿<?php 
+include 'inc/header.php';
+include 'inc/sidebar.php';
+?>
+<?php 
+include '../classes/Category.php';
+$ct = new Category();
+?>
+<?php
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+    $catName =$_POST['catName'];
+    $ctnpass = $ct->addCategory($catName);
+}
+?>
         <div class="grid_10">
             <div class="box round first grid">
                 <h2>Add New Category</h2>
+            <?php
+            if(isset($ctnpass)){
+                echo $ctnpass;
+            }?>
                <div class="block copyblock"> 
-                 <form>
+                 <form action ="" method = "post">
                     <table class="form">					
                         <tr>
                             <td>
-                                <input type="text" placeholder="Enter Category Name..." class="medium" />
+                                <input type="text" class="medium" placeholder="Enter Category Name..." name ="catName" />
                             </td>
                         </tr>
 						<tr> 
---
+++ classes/Category.php
@@ -0,0 +1,31 @@
+<?php
+include_once "../lib/Database.php";
+include_once "../helpers/Format.php";
+
+class Category{
+private $db;
+private $fm;
+public function __construct(){
+$this->db = new Database();
+$this->fm = new Format();
+}
+public function addCategory($catName){
+$catName = $this->fm->validation($catName);
+$catName = mysqli_real_escape_string($this->db->link , $catName);
+if($catName == ""){
+    $catadd_msg = "Category must not be empty .";
+    return $catadd_msg;
+}else{
+    $query  = "INSERT INTO tbl_category(catName) Values('$catName')";
+    $result = $this->db->insert($query); 
+    if($result){
+    $catadd_msg = "Category Inserted Successfully .";
+    return $catadd_msg;
+    }else{
+    $catadd_msg = "Category Not Inserted .";
+    return $catadd_msg;   
+    }    
+    }
+}
+//end brackets  
+}
