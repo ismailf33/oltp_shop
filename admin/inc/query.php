bd7623b89be2567b40cc12d7dc026b455862e343
alif33 <alifhasan332@gmail.com>
2019-06-20 21:23:18 +0600

    @all category query

--- admin/inc/query.php
+++ admin/inc/query.php
@@ -1,21 +1,21 @@
 <?php
 class query{
     public function addCategory($catName){
-    $query  = "INSERT INTO tbl_category(catName) Values('$catName')";   
+    $query  = "INSERT INTO tbl_category(catName) Values('$catName')";  // 
 }
 public function Category_list(){
-        $query  = "SELECT * FROM tbl_category";               
+        $query  = "SELECT * FROM tbl_category";  //             
     }
 public function Category_delete($delid){
-    $query  = "DELETE FROM tbl_category WHERE catId='$delid' "; 
+    $query  = "DELETE FROM tbl_category WHERE catId='$delid' "; //
 }
 public function Category_list($editid){ //By id
-    $query  = "SELECT * FROM tbl_category WHERE catId='$editid'";        
+    $query  = "SELECT * FROM tbl_category WHERE catId='$editid'"; //       
 }
 public function update_Category($catName,$editid){
     $query  = "UPDATE  tbl_category 
     SET catName = '$catName'
-    WHERE catId = '$editid' ";  
+    WHERE catId = '$editid' "; // 
 }
 //end 
 }
