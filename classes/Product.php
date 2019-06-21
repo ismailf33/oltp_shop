6ad64578bd5d6d96ad29c92101ac7b7b5f581d42
alif33 <alifhasan332@gmail.com>
2019-06-21 19:23:06 +0600

    .

--- classes/Product.php
+++ classes/Product.php
@@ -64,7 +64,7 @@ FROM tbl_product
 INNER JOIN tbl_category
 ON tbl_product.catId = tbl_category.catId
 INNER JOIN tbl_brand
-ON tbl_product.brandId = tbl_brand.	brandId
+ON tbl_product.brandId = tbl_brand.brandId
 ORDER BY tbl_product.productId DESC";
 $result = $this->db->select($query);
 return $result;
