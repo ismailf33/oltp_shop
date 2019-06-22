69900315442629f0789c9ab3cb4e738d209e2f2a
alif33 <alifhasan332@gmail.com>
2019-06-22 16:36:16 +0600

    @Session init here

--- inc/header.php
+++ inc/header.php
@@ -1,4 +1,8 @@
 <?php
+include "lib/Session.php";
+Session::init();
+?>
+<?php
   header("Cache-Control: no-cache, must-revalidate");
   header("Pragma: no-cache"); 
   header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); 
