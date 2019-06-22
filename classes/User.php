89157fee420fb5483a46c7e1c9f1973f3b77de15
alif33 <alifhasan332@gmail.com>
2019-06-22 16:34:39 +0600

    @font view part start

---
+++ classes/User.php
@@ -0,0 +1,14 @@
+<?php
+include_once "../lib/Database.php";
+include_once "../helpers/Format.php";
+
+class Cart{
+private $db;
+private $fm;
+public function __construct(){
+$this->db = new Database();
+$this->fm = new Format();
+}
+
+//end brackets
+}
--- inc/header.php
+++ inc/header.php
@@ -1,3 +1,9 @@
+<?php
+  header("Cache-Control: no-cache, must-revalidate");
+  header("Pragma: no-cache"); 
+  header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); 
+  header("Cache-Control: max-age=2592000");
+?>
 <!DOCTYPE HTML>
 <head>
 <title>Store Website</title>
