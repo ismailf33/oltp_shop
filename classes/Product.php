0de567cab596055ad007b534181f716930a16dc5
alif33 <alifhasan332@gmail.com>
2019-06-24 20:19:17 +0600

    @cart section start by alliasis query

--- classes/Product.php
+++ classes/Product.php
@@ -191,5 +191,25 @@ public function get_new_product(){
     $value = $this->db->select($query);
     return $value;
 
-}    //end brackets  
+}  
+public function each_item($id){       
+/*--INNER JOIN
+$query = "SELECT  tbl_product.*, tbl_category.catName, tbl_brand.brandName
+FROM tbl_product
+INNER JOIN tbl_category
+ON tbl_product.catId = tbl_category.catId
+INNER JOIN tbl_brand
+ON tbl_product.brandId = tbl_brand.brandId
+ORDER BY tbl_product.productId DESC";
+*/
+        //Alliasis(Banglai_Uponam)
+$query = "SELECT p.* ,c.catName, b.brandName
+FROM tbl_product as p, tbl_category as c,tbl_brand as b
+WHERE p.catId = c.catId AND p.brandId = b.brandId 
+
+ORDER BY p.productId DESC ";
+        $result = $this->db->select($query);
+        return $result;
+    }       
+  //end brackets  
 }
--- preview.php
+++ preview.php
@@ -2,20 +2,35 @@
 include "inc/header.php";
 include "inc/slider.php";
 ?>
+<?php
+if(! isset($_GET['proid']) || $_GET['proid'] == NULL){
+	echo "<script>window.location = '404.php';</script>";
+}else{
+	$id = $_GET['proid'];
+}
+if($_SERVER['REQUEST_METHOD'] == "POST" && isset($_POST['submit'])){
+$pro_up = $pro->update_Product($_POST , $_FILES ,$id);
+}
+?>
  <div class="main">
     <div class="content">
     	<div class="section group">
+
+	<?php
+	$eachid = $pd->each_item($id);
+	if($eachid){
+		while($value = $eachid->fetch_assoc()){ ?>
 				<div class="cont-desc span_1_of_2">				
 					<div class="grid images_3_of_2">
-						<img src="images/preview-img.jpg" alt="" />
+						<img src="admin/<?php echo $value['image']?>" alt="" />
 					</div>
 				<div class="desc span_3_of_2">
-					<h2>Lorem Ipsum is simply dummy text </h2>
+					<h2><?php echo $value['productName']?></h2>
 					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore.</p>					
 					<div class="price">
-						<p>Price: <span>$500</span></p>
-						<p>Category: <span>Laptop</span></p>
-						<p>Brand:<span>Samsnumg</span></p>
+						<p>Price: <span>$<?php echo $value['price']?></span></p>
+						<p>Category: <span><?php echo $value['catName']?></span></p>
+						<p>Brand:<span><?php echo $value['brandName']?></span></p>
 					</div>
 				<div class="add-cart">
 					<form action="cart.html" method="post">
@@ -31,6 +46,7 @@ include "inc/slider.php";
 	    </div>
 				
 	</div>
+	<?php	}} ?>
 				<div class="rightsidebar span_3_of_1">
 					<h2>CATEGORIES</h2>
 					<ul>
